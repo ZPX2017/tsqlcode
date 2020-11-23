bb41c89cc96bb49d5988f5c1269ac33745fae523
ZPX2017 <Xiao-Ping.Zhang@cdcr.ca.gov>
2020-11-23 15:59:11 -0800

    My first code file

---
+++ find_workbook.sql
@@ -0,0 +1,21 @@
+-- find site, project information from a workbook name
+select  w.id AS WOrkbook_id, 
+        w.name AS Workbook_name, 
+        w.repository_url,
+        w.owner_id as owner_user_id,
+        su.name as owner_name,
+        w.project_id, 
+        --u.system_user_id,
+        w.site_id,
+        s.name as site_name,
+        --p.id AS Project_id,
+        p.name AS Project_name,
+        w.created_at,
+        w.refreshable_extracts,
+        w.extracts_refreshed_at
+from workbooks w
+join projects p on P.id = w.project_id
+join sites s on s.id = w.site_id
+join users u on u.id = w.owner_id
+join system_users su on su.id = u.system_user_id 
+where w.name = 'BIS LMS Match (Courses)';
